532b7889ac4355761d97c8414385ad20bfced4be
Mikhail <mikhail3468@yandex.ru>
2023-06-07 05:32:08 +0300

    refactor(index.ts): reexport pages from index.ts

---
+++ client/src/pages/index.ts
@@ -0,0 +1,6 @@
+import Home from './Home';
+import ProductDetails from './ProductDetails';
+import Products from './Products';
+import Search from './Search';
+
+export { Home, ProductDetails, Products, Search };
