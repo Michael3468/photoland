8af1ee255120b0e2de39a6595bd4ed06dd9a23e7
Mikhail <mikhail3468@yandex.ru>
2023-06-17 10:42:40 +0300

    feat: add CategoryNavMobile component

--- client/src/components/CategoryNavMobile.tsx
+++ client/src/components/CategoryNavMobile.tsx
@@ -1,18 +1,33 @@
 import React, { FC, SetStateAction } from 'react';
 import { FiX } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import useFetch from '../hooks/useFetch';
+import { TCategory } from '../types';
 
 type Props = {
   setCatNavMobile: React.Dispatch<SetStateAction<boolean>>;
 };
 
-const CategoryNavMobile: FC<Props> = ({ setCatNavMobile }) => (
-  <div className="w-full h-full bg-primary p-8">
-    {/* close icon */}
-    <div className="flex justify-end mb-8 cursor-pointer" onClick={() => setCatNavMobile(false)}>
-      <FiX className="text-3xl" />
+const CategoryNavMobile: FC<Props> = ({ setCatNavMobile }) => {
+  const { data } = useFetch('/categories');
+  const categories: TCategory[] = data || [];
+
+  return (
+    <div className="p-8 w-full h-full bg-primary">
+      {/* close icon */}
+      <div className="flex justify-end mb-8 cursor-pointer" onClick={() => setCatNavMobile(false)}>
+        <FiX className="text-3xl" />
+      </div>
+      <div className="flex flex-col gap-y-8">
+        {categories.map((category) => (
+          <Link key={category.id} to={`/products/${category.id}`} className="font-medium uppercase">
+            {`${category.attributes.title} cameras`}
+          </Link>
+        ))}
+      </div>
     </div>
-    CategoryNavMobile
-  </div>
-);
+  );
+};
 
 export default CategoryNavMobile;
