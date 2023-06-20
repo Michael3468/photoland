b532d3e5e00a0c871f85d7155ddd6d93fed08b7e
Mikhail <mikhail3468@yandex.ru>
2023-06-20 14:40:28 +0300

    feat: add Products component

---
+++ client/src/pages/Products.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { CategoryNav, Product } from '../components';
+import { constants } from '../constants';
+import useFetch from '../hooks/useFetch';
+import { TProduct } from '../types';
+
+const Products = () => {
+  const { id } = useParams();
+  const { data } = useFetch(`/products?populate=*&filters[categories][id][$eq]=${id}`);
+  const products = data as unknown as TProduct[];
+  const [title, setTitle] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (data) {
+      setTitle(products[0].attributes.categories.data[0].attributes.title);
+    }
+  }, [data, products]);
+
+  return (
+    <div className={`mb-16 ${constants.themes.main.styles.contentBlock.marginTop}`}>
+      <div className="container mx-auto">
+        <div className="flex gap-x-[30px]">
+          <CategoryNav />
+          <main className="flex flex-col mx-auto">
+            {/* title */}
+            <div className="py-3 text-xl uppercase text-center lg:text-left">{`${title} cameras`}</div>
+
+            {/* product grid */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 lx:grid-cols-4 gap-[15px] md:gap-[30px]">
+              {products?.map((product) => (
+                <Product product={product} key={product.id} />
+              ))}
+            </div>
+          </main>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
